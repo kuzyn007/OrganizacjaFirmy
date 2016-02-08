12163d4fdc9f4a24c2db1ee3f8db20b9c5703ab9
Seweryn Adamczyk <seweryn.adamczyk88@gmail.com>
2016-02-08 19:27:59 +0100

    Switching to REST - in progress

--- src/main/java/pl/firma/controller/LinkController.java
+++ src/main/java/pl/firma/controller/LinkController.java
@@ -1,10 +1,9 @@
 package pl.firma.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class LinkController {
 	
 	@RequestMapping(value="/")
--- src/main/java/pl/firma/controller/TreeController.java
+++ src/main/java/pl/firma/controller/TreeController.java
@@ -3,17 +3,17 @@ package pl.firma.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import pl.firma.model.Tree;
 import pl.firma.service.TreeService;
 
-@Controller
+@RestController
 @RequestMapping(value="/tree")
 public class TreeController {
 	
