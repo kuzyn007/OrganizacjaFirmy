7e34e4d732115c3bcb4f7b382e4e52052d9095ea
Seweryn Adamczyk <seweryn.adamczyk88@gmail.com>
2016-02-08 19:45:26 +0100

    Switching to REST - in progress

--- src/main/java/pl/firma/controller/LinkController.java
+++ src/main/java/pl/firma/controller/LinkController.java
@@ -1,6 +1,7 @@
 package pl.firma.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
