ad19d64856cfa63807a9c6c779da7bae8dd2c5bf
Seweryn Adamczyk <seweryn.adamczyk88@gmail.com>
2016-02-09 11:11:32 +0100

    Added file RestConfig.class. Moved methods from WebAppConfig to RestConfig

---
+++ src/main/java/pl/firma/init/RestConfig.java
@@ -0,0 +1,21 @@
+package pl.firma.init;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+public class RestConfig {
+	@Bean
+	public View jsonTemplate() {
+		MappingJackson2JsonView view = new MappingJackson2JsonView();
+		view.setPrettyPrint(true);
+		return view;
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+		return new BeanNameViewResolver();
+	}
+}
--- src/main/java/pl/firma/init/WebAppConfig.java
+++ src/main/java/pl/firma/init/WebAppConfig.java
@@ -14,13 +14,9 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.BeanNameViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
-import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @Configuration
 @ComponentScan("pl.firma")
@@ -84,17 +80,5 @@ public class WebAppConfig {
 		resolver.setViewClass(JstlView.class);
 		return resolver;
 	}
-
-	@Bean
-    public View jsonTemplate() {
-        MappingJackson2JsonView view = new MappingJackson2JsonView();
-        view.setPrettyPrint(true);
-        return view;
-    }
-     
-    @Bean
-    public ViewResolver viewResolver() {
-        return new BeanNameViewResolver();
-    }
 }
 
