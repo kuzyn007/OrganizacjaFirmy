6e6509b0a5dbe33d3016c5add95b03b4f0987beb
Seweryn Adamczyk <seweryn.adamczyk88@gmail.com>
2016-02-09 11:14:26 +0100

    Added line @Configuration into RestConfig

--- src/main/java/pl/firma/init/RestConfig.java
+++ src/main/java/pl/firma/init/RestConfig.java
@@ -1,11 +1,13 @@
 package pl.firma.init;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.BeanNameViewResolver;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
+@Configuration
 public class RestConfig {
 	@Bean
 	public View jsonTemplate() {
